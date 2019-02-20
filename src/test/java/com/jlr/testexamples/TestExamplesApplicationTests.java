c3b495946d825c488d942f23332c1552f60cd191
cspence8 <cspence8@jaguarlandrover.com>
2019-02-20 14:13:50 +0000

    Set up environment to create springboot test examples with Junit and Mockito frameworks.

---
+++ src/test/java/com/jlr/testexamples/TestExamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jlr.testexamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestExamplesApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
